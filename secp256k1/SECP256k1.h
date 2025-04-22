cd1145620ab4a18337f43c991540df67df447135
Adz2323 <146026081+Adz2323@users.noreply.github.com>
2025-04-22 21:14:47 +0100

    Update SECP256k1.h

--- secp256k1/SECP256k1.h
+++ secp256k1/SECP256k1.h
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 
 #ifndef SECP256K1H
 #define SECP256K1H
@@ -22,43 +22,44 @@
 #include <vector>
 
 // Address type
-#define P2PKH 0
-#define P2SH 1
+#define P2PKH  0
+#define P2SH   1
 #define BECH32 2
 
-class Secp256K1
-{
+
+class Secp256K1 {
 
 public:
+
   Secp256K1();
   ~Secp256K1();
-  void Init();
+  void  Init();
   Point ComputePublicKey(Int *privKey);
   Point NextKey(Point &key);
-  bool EC(Point &p);
-  Int GetY(Int x, bool isEven);
-
+  bool  EC(Point &p);
+  
   Point ScalarMultiplication(Point &P, Int *scalar, bool debug = false);
   bool VerifyPoint(Point &p, const char *label);
   Point GetIdentity();
+  
+  char* GetPublicKeyHex(bool compressed, Point &p);
+  void GetPublicKeyHex(bool compressed, Point &pubKey,char *dst);
+  
+  char* GetPublicKeyRaw(bool compressed, Point &p);
+  void GetPublicKeyRaw(bool compressed, Point &pubKey,char *dst);
+  
+  bool ParsePublicKeyHex(char *str,Point &p,bool &isCompressed);
 
-  char *GetPublicKeyHex(bool compressed, Point &p);
-  void GetPublicKeyHex(bool compressed, Point &pubKey, char *dst);
+  void GetHash160(int type,bool compressed,
+    Point &k0, Point &k1, Point &k2, Point &k3,
+    uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
 
-  char *GetPublicKeyRaw(bool compressed, Point &p);
-  void GetPublicKeyRaw(bool compressed, Point &pubKey, char *dst);
+  void GetHash160(int type,bool compressed, Point &pubKey, unsigned char *hash);
+  
+  void GetHash160_fromX(int type,unsigned char prefix,
+  Int *k0,Int *k1,Int *k2,Int *k3,
+  uint8_t *h0,uint8_t *h1,uint8_t *h2,uint8_t *h3);
 
-  bool ParsePublicKeyHex(char *str, Point &p, bool &isCompressed);
-
-  void GetHash160(int type, bool compressed,
-                  Point &k0, Point &k1, Point &k2, Point &k3,
-                  uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
-
-  void GetHash160(int type, bool compressed, Point &pubKey, unsigned char *hash);
-
-  void GetHash160_fromX(int type, unsigned char prefix,
-                        Int *k0, Int *k1, Int *k2, Int *k3,
-                        uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
 
   Point Add(Point &p1, Point &p2);
   Point Add2(Point &p1, Point &p2);
@@ -66,14 +67,18 @@ public:
   Point Double(Point &p);
   Point DoubleDirect(Point &p);
   Point Negation(Point &p);
+  Point SubtractPoints(Point &P1, Point &P2);
 
-  Point G;   // Generator
-  Int P;     // Prime for the finite field
-  Int order; // Curve order
+  Point G;                 // Generator
+  Int P;                   // Prime for the finite field
+  Int   order;             // Curve order
 
 private:
-  uint8_t GetByte(char *str, int idx);
-  Point GTable[256 * 32]; // Generator table
+
+  uint8_t GetByte(char *str,int idx);
+  Int GetY(Int x, bool isEven);
+  Point GTable[256*32];       // Generator table
+
 };
 
 #endif // SECP256K1H
